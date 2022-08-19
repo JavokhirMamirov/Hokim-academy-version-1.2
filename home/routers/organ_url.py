364368fc9cf2d02ef9d27896b8133323530ae417
DADAJONOVB <dadajonovboburjon3@gmail.com>
2022-08-19 14:53:24 +0300

    schol(create-update-list-detail)

--- home/routers/organ_url.py
+++ home/routers/organ_url.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from home.viewset.auth_view import admin_login, admin_logout, page_404
-from home.viewset.organization_vew import organ_dashboard_view
+from home.viewset import organization_vew
 
 organization_url = [
 
-    path('dashboard/', organ_dashboard_view, name='organ-dashboard')
+    path('dashboard/', organization_vew.organ_dashboard_view, name='organ-dashboard'),
+    path('schools/', organization_vew.schools_list_view, name='schools'),
+    path('school/add/', organization_vew.add_schools_view, name='add-school'),
+    path('school/detail/<int:pk>/', organization_vew.school_detail_view, name='school-detail'),
+    path('school/update/<int:pk>/', organization_vew.update_detail_view, name='update-school')
 
 ]
--- home/viewset/organization_vew.py
+++ home/viewset/organization_vew.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from account.models import School, City
 from home.decarators import organ_required
 
 
@@ -8,3 +8,82 @@ from home.decarators import organ_required
 @organ_required
 def organ_dashboard_view(request):
     return render(request, 'oranization/index.html')
+
+def schools_list_view(request):
+    schools = School.objects.all()
+    citys = City.objects.all()
+    context = {
+        'schools':schools,
+        'citys':citys
+    }
+    return render(request, 'oranization/schools.html', context)
+
+
+def add_schools_view(request):
+    citys = City.objects.all()
+    context = {
+        'citys': citys
+    }
+    if request.method == 'POST':
+        name = request.POST['name']
+        director = request.POST['director']
+        address = request.POST['address']
+        city_id = request.POST['city']
+        website = request.POST['website']
+        facebook = request.POST['facebook']
+        instagram = request.POST['instagram']
+        telegram = request.POST['telegram']
+        city = City.objects.get(id=city_id)
+        school = School.objects.create(
+            name=name,
+            director=director,
+            address=address,
+            city=city,
+            website=website,
+            facebook=facebook,
+            instagram=instagram,
+            telegram=telegram
+        )
+
+    return render(request, 'oranization/add-school.html', context)
+
+def school_detail_view (request, pk) :
+    school = School.objects.get(pk=pk)
+    context = {
+        'school': school
+    }
+    return render(request, 'oranization/school-detail.html', context)
+
+def update_detail_view(request, pk):
+    school = School.objects.get(pk=pk)
+    citys = City.objects.all()
+    if request.method == 'POST':
+        name = request.POST['name']
+        director = request.POST['director']
+        address = request.POST['address']
+        city_id = request.POST['city']
+        website = request.POST.get('website')
+        facebook = request.POST.get('facebook')
+        instagram = request.POST.get('instagram')
+        telegram = request.POST.get('telegram')
+        city = City.objects.get(id=city_id)
+        school.name = name
+        school.director = director
+        school.address = address
+        school.city = city
+        school.website = website
+        school.facebook = facebook
+        school.instagram = instagram
+        school.telegram = telegram
+        school.save()
+        return redirect('school-detail', school.id)
+    context = {
+        'school': school,
+        'citys': citys
+    }
+    return render(request, 'oranization/update-school.html', context)
+
+def school_delete_view(request, pk):
+    school = School.objects.get(id=pk)
+    school.delete()
+    return redirect('schools')
