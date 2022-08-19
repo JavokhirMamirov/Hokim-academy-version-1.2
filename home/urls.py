3b27a513cbea9560c618c2a97dda61c1eefea1cd
Mamirov1997 <mamirovjavoxir@gmail.com>
2022-08-19 18:09:16 +0500

    template

---
+++ home/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+
+organization_url = [
+
+]
+
+school_url = [
+
+]
+
+student_url = [
+
+]
+
+auth_url = [
+
+]
+
+urlpatterns = [
+
+              ] + organization_url + student_url + school_url + auth_url
