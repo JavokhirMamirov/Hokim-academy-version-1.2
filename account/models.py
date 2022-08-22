4c1f5858bc1f6be76f54d0e3ae06e7f598c89967
Mamirov1997 <mamirovjavoxir@gmail.com>
2022-08-22 13:11:33 +0500

    generated certificate

--- account/models.py
+++ account/models.py
@@ -3,9 +3,18 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 from random import randint
+import secrets
+import string
+
+from home.packages.certificate_generate import certificate_generate
 
 
-# Create your models here.
+def generate_promokod():
+    alphabet = string.ascii_letters + string.digits
+    password = ''.join(secrets.choice(alphabet) for i in range(8))
+
+    return password
+
 
 class City(models.Model):
     name = models.CharField(max_length=255)
@@ -62,19 +71,25 @@ class Student(models.Model):
     image = models.ImageField(upload_to='avatars/', null=True, blank=True)
     subject = models.ForeignKey(Subject, on_delete=models.SET_NULL, null=True, blank=True)
     status = models.SmallIntegerField(default=1, choices=STUDENT_TYPES)
+    promo_code = models.CharField(max_length=10, null=True, blank=True)
     username = models.CharField(max_length=255, unique=True)
+    certificate = models.ImageField(upload_to='certificate/', null=True, blank=True)
     password = models.CharField(max_length=255)
     school = models.ForeignKey(School, on_delete=models.SET_NULL, null=True, blank=True)
+    is_used_promocode = models.BooleanField(default=False)
 
     def save(self, *args, **kwargs):
         if self.pk is None:
-            self.password = make_password('ha-2022')
+            self.promo_code = generate_promokod()
+            self.password = make_password(self.promo_code)
+
             while True:
                 username = f'student-{randint(1, 9999999)}'
                 query = Student.objects.filter(username=username)
                 if query.count() == 0:
                     self.username = username
                     break
+            self.certificate = certificate_generate(self)
         super(Student, self).save(*args, **kwargs)
 
     def __str__(self):
