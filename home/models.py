d1f273db7cc4d75f243899f552b81e2f70c44ded
Mamirov1997 <mamirovjavoxir@gmail.com>
2022-09-24 12:11:57 +0500

    school and teacher ariza

--- home/models.py
+++ home/models.py
@@ -5,7 +5,7 @@ from account.models import City
 
 
 class SchoolAriza(models.Model):
-    city = models.ForeignKey(City, on_delete=models.CASCADE)
+    city = models.CharField(max_length=255, null=True, blank=True)
     name = models.CharField(max_length=255)
     phone = models.CharField(max_length=25, null=True, blank=True)
     text = models.TextField()
